92f34b9060d861fde1d89d5e5bd2f29ca2f18228
Riyaz Jaleel <riyazj82@gmail.com>
2021-12-22 14:10:25 +0530

    Third commit

--- main.tf
+++ main.tf
@@ -19,3 +19,7 @@ resource "aws_s3_bucket" "bucket" {
     
   }
 }
+resource "aws_cognito_identity_pool" "main" {
+  identity_pool_name               = "photoviewer"
+  allow_unauthenticated_identities = true
+}
