241b0515e58caff70efb4289dd0b0f7a3605b9ee
Riyaz Jaleel <riyazj82@gmail.com>
2021-12-22 14:08:40 +0530

    second commit

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ terraform {
 provider "aws" {
   region = "ap-south-1"
 }
+resource "aws_s3_bucket" "bucket" {
+  bucket = "photoviewer-bucket"
+  acl    = "public-read-write"
+ tags = {
+    Name        = "photoviewer-bucket"
+    
+  }
+}
