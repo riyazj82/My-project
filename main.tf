479fcac7a0438ce623354270f898a077747d60e2
Riyaz Jaleel <riyazj82@gmail.com>
2021-12-23 23:36:07 +0530

    linked pool to role

--- main.tf
+++ main.tf
@@ -49,3 +49,10 @@ resource "aws_iam_role" "unauthenticated" {
 }
 EOF
 }
+
+resource "aws_cognito_identity_pool_roles_attachment" "main" {
+  identity_pool_id = aws_cognito_identity_pool.main.id
+  roles = {
+    "unauthenticated" = aws_iam_role.unauthenticated.arn
+  }
+}
