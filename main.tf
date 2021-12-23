0f2fb18f91334f62b39b7050fe7bf47c03244406
Riyaz Jaleel <riyazj82@gmail.com>
2021-12-23 22:57:55 +0530

    forth commit

--- main.tf
+++ main.tf
@@ -23,3 +23,23 @@ resource "aws_cognito_identity_pool" "main" {
   identity_pool_name               = "photoviewer"
   allow_unauthenticated_identities = true
 }
+resource "aws_iam_role" "unauthenticated" {
+  name = "cognito_unauthenticated"
+
+  assume_role_policy = <<EOF
+  {
+   "Version": "2012-10-17",
+   "Statement": [
+      {
+         "Effect": "Allow",
+         "Action": [
+            "s3:ListBucket"
+         ],
+         "Resource": [
+            "arn:aws:s3:::photoviewer-bucket"
+         ]
+      }
+   ]
+}
+EOF
+}
